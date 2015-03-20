8c4bcdf74fcb44869e1a34a1180471a5151be443
J. TURNER <turner.j6@gmail.com>
2015-03-20 14:20:08 -0700

    Correct redirect_to path ('/' --> '/users').

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,10 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       flash[:notice] = "Your user name has been established."
-      redirect_to "/"
+      redirect_to "/users"
     else
       flash[:alert] = "!ERROR! Please establish a user name."
+      redirect_to :back
     end
   end
 
