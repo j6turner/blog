ae79cbc43d6cc61c3712548758f832f17363d9e7
J. TURNER <turner.j6@gmail.com>
2015-03-20 13:50:35 -0700

    Add 'content' column to 'posts' table.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
   validates :user_id, :presence => true
   validates :name, :presence => true
+  validates :content, :presence => true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150320175800) do
+ActiveRecord::Schema.define(version: 20150320204048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150320175800) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "content"
   end
 
   create_table "users", force: :cascade do |t|
