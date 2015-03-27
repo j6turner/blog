65b08f5e2bd7ec0b21b91a0392b26c7fb3e47876
J. TURNER <turner.j6@gmail.com>
2015-03-27 11:41:05 -0700

    Modify files to include authentication components (email, password, confirmation).

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,30 @@
 class User < ActiveRecord::Base
   has_many :posts
+  attr_accessor :password
+
   validates :name, :presence => true
+  validates :email, :presence => true
+  validates :password, :presence => true
+  validates :password_confirmation, :presence => true
+  validates_confirmation_of :password
+  before_save :encrypt_password
+
+  def encrypt_password
+    self.password_salt = BCrypt::Engine.generate_salt
+    self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+  end
+
+  def true_email?
+    self.email.include?('@') && self.email.include('.')
+  end
+
+  def self.authenticate(email, password)
+    user = User.where(email: email).first
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nil
+    end
+  end
+
 end
