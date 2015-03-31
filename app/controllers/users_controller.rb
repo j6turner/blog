0744c79e23ac7b41da3f5f8187dce366ce63a5d4
J. TURNER <turner.j6@gmail.com>
2015-03-30 19:13:48 -0700

    Update README.rdoc.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,6 @@ class UsersController < ApplicationController
   end
 
   def create
-    binding.pry
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Your user name has been established."
